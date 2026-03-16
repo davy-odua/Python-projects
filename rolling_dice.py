367c406f2991decb09f246a86fe7ba462d35e1c5
Davy-odua <davyodua@gmail.com>
2026-03-16 19:16:28 +0300

    dice

--- rolling_dice.py
+++ rolling_dice.py
@@ -1,9 +1,33 @@
 #Create a dice which prints random numbers.
-
-#Question - Enter the number (y or n)
+#1. Question - Enter the number (y or n)
 #when we enter y we should generate the random numbers
-#when we enter n we should print (Thanks) and terminate
+#Loop when you enter y
+#2. When we enter n we should print (Thanks) and terminate
 #when we enter another string which is not mentioned then print(Invalid choice)
+#loop when you enter an invalid choice.
+import random
+
+dicing = True
+while dicing:
+    dice = input("Enter the number? (y/n): ")
+    if dice == "y":
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+        print(f"({dice1}, {dice2})")
+    elif dice == "n":
+        print("Thanks for playing!")
+        break
+    else:
+        print("Invalid choice")
+
+
+
+
+
+
+
+
+
 
 
 
