223a481343624a589aa78fb02454a73285b4ab9b
Davy-odua <davyodua@gmail.com>
2026-03-21 06:33:09 +0300

    rock_paper_scissors

--- rolling_dice.py
+++ rolling_dice.py
@@ -5,20 +5,22 @@
 #2. When we enter n we should print (Thanks) and terminate
 #when we enter another string which is not mentioned then print(Invalid choice)
 #loop when you enter an invalid choice.
-# import random
-#
-# dicing = False
-# while not dicing:
-#     dice = input("Enter the number? (y/n): ").lower()
-#     if dice == "y":
-#         dice1 = random.randint(1, 6)
-#         dice2 = random.randint(1, 6)
-#         print(f"({dice1}, {dice2})")
-#     elif dice == "n":
-#         print("Thanks for playing!")
-#         break
-#     else:
-#         print("Invalid choice")
+
+
+import random
+
+dicing = False
+while not dicing:
+    dice = input("Enter the number? (y/n): ").lower()
+    if dice == "y":
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+        print(f"({dice1}, {dice2})")
+    elif dice == "n":
+        print("Thanks for playing!")
+        break
+    else:
+        print("Invalid choice")
 
 
 
